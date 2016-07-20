51949ac9b7972cabd5b95a423887741825312282
taraojo <tara.ojo@gmail.com>
2016-07-20 21:30:02 +0100

    add random motivational quote picker

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 //routes
 // accepting a request and sending out a response
 const express = require('express');
+const motivations = require('motivations'); //array of statements
+const pickOne = require('pick-one');
+
+
 const app = express();
 
 app.get('/hello', function (request, response) {
     console.log('request to /hello');
-    response.send('Hello');
+    var motivator = pickOne(motivations);
+    response.send(motivator);
 });
 
 module.exports = app;
